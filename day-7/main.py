bbd900e347851022f337ac887826a0bc66191520
MaximumMaxxx <77695343+MaximumMaxxx@users.noreply.github.com>
2021-12-06 21:20:02 -0800

    Added sketchy ass solution for day 7

--- day-7/main.py
+++ day-7/main.py
@@ -1,1 +1,34 @@
-input_raw = open("input.txt","r")
+input = open("input.txt","r")
+inputraw = input.read()
+
+crabs = inputraw.split(",")
+
+crabs = [int(crab) for crab in crabs]
+
+max_crab = 0
+for i in crabs:
+    if i > max_crab:
+        max_crab = i
+
+print(max_crab)
+
+best_fuel = 99999999999999999999999999999999999
+for i in range(max_crab):
+    print(i)
+    total_fuel = 0
+    for crab in crabs:
+        crab_fuel_cost = 1
+        while crab != i:
+            if crab < i:
+                total_fuel+=crab_fuel_cost
+                crab += 1
+                crab_fuel_cost += 1
+            else:
+                total_fuel+=crab_fuel_cost
+                crab -= 1
+                crab_fuel_cost += 1
+
+    if total_fuel < best_fuel:
+        best_fuel = total_fuel
+    
+print(f"The best fuel is {best_fuel}")
