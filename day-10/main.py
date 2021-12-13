5483a8ab5f87a734fe6be14fedb5560c7fa13548
MaximumMaxxx <77695343+MaximumMaxxx@users.noreply.github.com>
2021-12-12 21:51:40 -0800

    Added partial solution for day 10

--- day-10/main.py
+++ day-10/main.py
@@ -2,23 +2,69 @@ with open("input.txt","r") as f:
     input = f.read().strip().split("\n")
 
 openchars = ["(","[","{","<"]
-closechars = [")","]","}",">"]
-scorelookuptable = {"(":3,"[":57,"{":1197,"<":25137}
+closecgars = ["Null",")","]","}",">"]
+closecharmatches = {")":"(","}":"{","]":"[",">":"<"}
+
+addon_stack_lookuptable = {"(":")","{":"}","<":">","[":"]"}
+
+scorelookuptable = {")":3,"]":57,"}":1197,">":25137}
 score = 0
 
 for line in input:
-    squares,circles,curly,arrows = 0,0,0,0
+    stack = []
     for char in line:
-        if char == "(": circles += 1
-        if char == ")": circles -= 1
-        if char == "[": squares += 1
-        if char == "]": squares -= 1
-        if char == "{": curly += 1
-        if char == "}": curly -= 1
-        if char == "<": arrows += 1
-        if char == ">": arrows -= 1
-    print(circles,squares,curly,arrows)
+        if char in openchars: stack.append(char)
+        else: 
+            if closecharmatches[char] == stack[len(stack)-1]: stack.pop(len(stack)-1)
+            else: 
+                score += scorelookuptable[char]
+                break
+
+print(f"The final score for part 1 is {score}")
+
+scores = []
+for line in input:
+    stack = []
+
+    # Generating the stack
+    for char in line:
+        if char in openchars: stack.append(char)
+        else: 
+            if closecharmatches[char] == stack[len(stack)-1]: stack.pop(len(stack)-1)
+            else:
+                break
+    addon_stack = []
+
+    # Gnerating the add on stack
+    for char in stack:
+        addon_stack.append(addon_stack_lookuptable[char])
+    localscore = 0
 
 
+    addon_stack_copy = addon_stack.copy()
+    mathorder = []
 
-print(f"The final score is {score}")
+    # Generating the order of the elements
+    for item in addon_stack_copy:
+        print(addon_stack)
+        mathorder.append(item)
+        for i in addon_stack_copy:
+            if i == item: addon_stack_copy.remove(item)
+    localscore = 0
+    print(mathorder)
+
+    # Generating the local score
+    for index,value in enumerate(mathorder):
+        localscore *= 5
+        try:tempscore = openchars[openchars.index(value)] * addon_stack.count(value)
+        except: tempscore =0 
+        print(tempscore)
+        localscore += tempscore
+        addon_stack.remove(value)
+    print(localscore)
+    scores.append(localscore)
+
+scores.sort()
+
+print(scores)
+
